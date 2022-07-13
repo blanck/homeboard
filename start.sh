9613b9ca04401d76b6555f538868aaafd90c9c6c
blanck <jonas@blanck.hk>
2022-07-13 16:49:07 +0200

    fix startup script

--- start.sh
+++ start.sh
@@ -6,13 +6,15 @@ unclutter -idle 0 &
 
 xset -display :0.0 dpms force on; xset -display :0.0 -dpms
 
-cd ~/homeboard/ && git reset HEAD --hard
+cd ~/homeboard/ && git fetch origin master
+cd ~/homeboard/ && git reset --hard origin/master
 cd ~/homeboard/ && git pull --no-edit https://github.com/blanck/homeboard.git
+
 # npm install
 sleep 1
 cd ~/homeboard/ && npm run build-prod
 sleep 5
 pm2 restart server
-#pm2 restart "npm run dev"
+# pm2 restart "npm run dev"
 sleep 30
 chromium-browser --kiosk --start-fullscreen --app=http://localhost:8080 &
